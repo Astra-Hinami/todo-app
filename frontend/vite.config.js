7e6c8552a07f0fe7ebb3b435d714dc901d4f2cd5
Astra-Hinami <astra.hinami@gmail.com>
2025-01-30 16:56:20 +0530

    path

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'frontend',  // Set the root to the 'frontend' folder
-  build: {
-    outDir: 'dist',  // Ensure this matches what Render expects
-  }
-});
